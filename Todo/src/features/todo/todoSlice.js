d4cdc9e1d9694eefeebe55e9b1d21e000597f223
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2025-02-05 12:33:31 +0530

    setting up Redux ToolKit

---
+++ Todo/src/features/todo/todoSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: "abc", task: "demo_task", isDone: false }],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    // have 2 things state & action
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: nanoid(),
+        task: action.payload,
+        isDone: false,
+      };
+      state.todos.push(newTodo); // direact Mutation is possiable with the ToolKit
+    },
+
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+
+    markAsDone: (state, action) => {
+        state.todos = state.todos.map((todo) => {
+            if(todo.id === action.payload) {
+                todo.isDone = true;
+            }
+        });
+    },
+  },
+});
+
+// to let redux toolkit do its job by creating Actions
+export const { addTodo, deleteTodo, markAsDone } = todoSlice.actions;
+export default todoSlice.reducer;
